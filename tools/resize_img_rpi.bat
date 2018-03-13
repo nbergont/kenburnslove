8bf2b5e9aa9a869e899ff38190c4a149cfc54d75
nbergont <you@example.com>
2018-03-13 19:18:04 +0100

    XnConvert script for resize rpi images

--- tools/resize_img_rpi.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set root=%cd%
-:treeProcess
-%root%\magick.exe mogrify -resize 2048x2048 -monitor List *.jpg
-for /f "delims=" %%i in ('dir /ad/s/b') do (
-	cd %%i
-	call :treeProcess
-	cd ..
-)
